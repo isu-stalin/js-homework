886fe0edcb3b815a964b06b611f6d6d9379521b1
Xamrayev Xasan <xamrayevxasan11@gmail.com>
2023-06-02 13:22:59 +0500

    js/script.js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let myNameIs = "Mening ismim"
+let name = "Xasan"
+
+console.log(myNameIs + " > " + name);
+
+
+
+let mySurnameIs = "Familiyam"
+let surname = "Xamrayev"
+
+console.log(mySurnameIs + " > " + surname);
+
+
+
+let myAge = "Yoshim"
+let age = "16-da"
+
+console.log(myAge + " > " + age);
+
+
+
+let imStudy = "Maktabim"
+let mySchool = "12"
+
+console.log(imStudy + " > " + mySchool)
+
+
+
+let myJob = "Kasbim"
+let student = "O'quvchi"
+
+console.log(myJob + " > " + student);
